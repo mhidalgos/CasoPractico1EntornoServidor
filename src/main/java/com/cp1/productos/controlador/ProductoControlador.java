03ebdf77bec13d5e0d8a79b125ec3b91a104217b
mhidalgos <161379303+mhidalgos@users.noreply.github.com>
2024-10-19 20:14:49 +0200

    Crear y guardar producto

--- src/main/java/com/cp1/productos/controlador/ProductoControlador.java
+++ src/main/java/com/cp1/productos/controlador/ProductoControlador.java
@@ -1,18 +1,34 @@
 package com.cp1.productos.controlador;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cp1.productos.modelo.Producto;
+import com.cp1.productos.servicios.ProductoServicio;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoControlador {
+	
+	@Autowired
+	private ProductoServicio productoServicio;
 	
 	@GetMapping("")
 	public String mostrar() {
 		return "productos/mostrar";
 	}
 	
-		
+	@GetMapping("/crear")
+	public String crear () {
+		return "productos/crear";
+	}
 
+	@PostMapping("/guardar")
+	public String guardar(Producto producto) {
+		productoServicio.save(producto);
+		return "redirect:/productos";
+	}
 }
